6a0eaae7dadb4fea2558661691fcd66049e15644
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2021-02-01 19:47:48 -0600

    button

---
+++ pomodoro_project/src/components/Button.js
@@ -0,0 +1,7 @@
+const Button = ({id, children, onClick}) => {
+    return(
+        <button id={id} onClick={onClick}>{children}</button>
+    )
+}
+
+export default Button
---
+++ pomodoro_project/src/components/clock/Clock.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Button from '../button/Button';
+
+return (
+    Time left: 25:00
+    <Button id={...} onClick={,...}>....</Button>
+);
